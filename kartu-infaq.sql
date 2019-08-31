291bea1d8902446fd8eb03e78d58b2e2f8d9c61c
Henri Susanto <henry.dinus@gmail.com>
2019-08-31 19:00:51 +0700

    filter kartu infaq by month year

--- kartu-infaq.sql
+++ kartu-infaq.sql
@@ -7,5 +7,7 @@ LEFT JOIN jamaah ON struk.jamaah = jamaah.`uuid`
 LEFT JOIN strukdetail ON struk.`uuid` = strukdetail.struk
 LEFT JOIN jatahdesadetail ON strukdetail.jatahDesaDetail = jatahdesadetail.`uuid`
 LEFT JOIN infaq ON jatahdesadetail.infaq = infaq.`uuid`
+LEFT JOIN jatahdesa ON jatahdesa.`uuid` = jatahdesadetail.jatahDesa
 WHERE TRUE
+AND MONTH(jatahdesa.tanggal) = 9 AND YEAR(jatahdesa.tanggal) = 2019
 ORDER BY struk.jamaah, dibulatkan, infaq.nama
